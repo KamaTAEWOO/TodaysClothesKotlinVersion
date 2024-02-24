e5de29fba5d001c9a6cdf2d2eb8ec97c3bfcc6d6
kamamerona <zszs2020@naver.com>
2024-02-24 22:56:50 +0900

    [feat] hilt 연결 및 timber 연결

---
+++ app/src/main/java/com/kama/todaysclothes_kotlin/AppDebugTree.kt
@@ -0,0 +1,9 @@
+package com.kama.todaysclothes_kotlin
+
+import timber.log.Timber
+
+class AppDebugTree : Timber.DebugTree() {
+    override fun createStackElementTag(element: StackTraceElement): String? {
+        return "${element.fileName}:${element.lineNumber}#${element.methodName}"
+    }
+}
--- app/src/main/java/com/kama/todaysclothes_kotlin/MainActivity.kt
+++ app/src/main/java/com/kama/todaysclothes_kotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kama.todaysclothes_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity::"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * 앱 시작
+         * */
+        Timber.d("$TAG::앱 시작")
+        //startActivity(Intent(this, SplashActivity::class.java))
     }
 }
