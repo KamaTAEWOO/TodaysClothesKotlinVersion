7e0a31f62cbd6a23dcb5952834d226118e08c30e
kamamerona <zszs2020@naver.com>
2024-03-03 15:21:03 +0900

    [feat] 날씨 데이터 받기 완료

--- presentation/src/main/java/com/kama/presentation/view/WeatherFragment.kt
+++ presentation/src/main/java/com/kama/presentation/view/WeatherFragment.kt
@@ -2,8 +2,12 @@ package com.kama.presentation.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.kama.core.base.BaseFragment
+import com.kama.design.R
 import com.kama.presentation.databinding.FragmentWeatherBinding
+import com.kama.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -11,16 +15,53 @@ import timber.log.Timber
 class WeatherFragment : BaseFragment<FragmentWeatherBinding>() {
 
     private val TAG = "WeatherFragment::"
+    private lateinit var mainViewModel: MainViewModel
 
     override fun getFragmentBinding(): FragmentWeatherBinding =
         FragmentWeatherBinding.inflate(layoutInflater)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initViewModel()
         init()
+    }
+
+    private fun initViewModel() {
+        Timber.i("$TAG::initViewModel()")
+        mainViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
     }
 
     private fun init() {
         Timber.i("$TAG::init()")
+        weatherDataReceive()
+        swipeRefresh()
     }
+
+    private fun weatherDataReceive() {
+        Timber.i("$TAG::weatherDataReceive()")
+
+        var data = ""
+        mainViewModel.result.observe(viewLifecycleOwner) { resultData ->
+            Timber.d("$TAG::result.observe() $resultData")
+            if (resultData) {
+                mainViewModel.weatherData.forEach {
+                    if (it.baseDate == it.fcstDate) {
+                        data += "${it.category} -> ${it.fcstValue} \n"
+                    }
+                }
+                binding.textView.text = data
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.waiting_data), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun swipeRefresh() {
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            Toast.makeText(requireContext(), "스와이프 완료", Toast.LENGTH_SHORT).show()
+            binding.swipeRefreshLayout.isRefreshing = false // 새로고침 완료
+        }
+    }
+
 }
