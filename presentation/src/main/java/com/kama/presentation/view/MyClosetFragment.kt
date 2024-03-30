366e6c7f65bcaa13a81193cea02619c56fed776c
kamamerona <zszs2020@naver.com>
2024-03-30 17:29:03 +0900

    [feat] 버튼 클릭 시 이미지 들고 와서 보여주는 로직 완료

--- presentation/src/main/java/com/kama/presentation/view/MyClosetFragment.kt
+++ presentation/src/main/java/com/kama/presentation/view/MyClosetFragment.kt
@@ -1,10 +1,16 @@
 package com.kama.presentation.view
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.kama.core.base.BaseFragment
 import com.kama.presentation.R
 import com.kama.presentation.databinding.FragmentMyClosetBinding
@@ -31,5 +37,27 @@ class MyClosetFragment : BaseFragment<FragmentMyClosetBinding>() {
 
     private fun init() {
         Timber.i("$TAG::init()")
+
+        binding.addButton.setOnClickListener {
+            openAlbum()
+        }
+    }
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val imageUri = result.data?.data ?: return@registerForActivityResult
+            binding.addButton.setImageURI(imageUri)
+
+            // 이미지뷰의 스케일 타입 설정
+            binding.addButton.scaleType = ImageView.ScaleType.CENTER_CROP
+            // 이미지뷰의 배경색 설정 화이트
+            binding.addButton.setBackgroundColor(resources.getColor(com.kama.design.R.color.white, null))
+        }
+    }
+
+    private fun openAlbum() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        pickImage.launch(intent)
     }
 }
