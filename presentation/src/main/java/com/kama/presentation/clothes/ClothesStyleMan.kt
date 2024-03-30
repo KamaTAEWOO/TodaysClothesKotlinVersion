7df128339a500a7d1fe4d1568c424601ea2a8665
kamamerona <zszs2020@naver.com>
2024-03-30 14:46:40 +0900

    [feat] Recommend Clothes 정리 완료, 호출할 수 있는 함수 완료

--- presentation/src/main/java/com/kama/presentation/clothes/ClothesStyleMan.kt
+++ presentation/src/main/java/com/kama/presentation/clothes/ClothesStyleMan.kt
@@ -15,6 +15,229 @@ internal class ClothesStyleMan {
         }
     }
 
+    // String 값이 들어오면 해당 변수의 값을 intArrayOf로 반환
+    fun getDandyStyle(style: String): IntArray {
+        return when (style) {
+            "AAD4" -> AAD4
+            "AAD5" -> AAD5
+            "AAD9" -> AAD9
+            "AAD12" -> AAD12
+            "AAD17" -> AAD17
+            "AAD20" -> AAD20
+            "AAD23" -> AAD23
+            "AAD28" -> AAD28
+            "ABD4" -> ABD4
+            "ABD5" -> ABD5
+            "ABD9" -> ABD9
+            "ABD12" -> ABD12
+            "ABD17" -> ABD17
+            "ABD20" -> ABD20
+            "ABD23" -> ABD23
+            "ABD28" -> ABD28
+            else -> intArrayOf()
+        }
+    }
+
+    // String 값이 들어오면 해당 text의 값을 StringArray로 반환
+    fun getDandyStyleText(style: String): Array<String> {
+        return when (style) {
+            "AAD4" -> txt_AAD4
+            "AAD5" -> txt_AAD5
+            "AAD9" -> txt_AAD9
+            "AAD12" -> txt_AAD12
+            "AAD17" -> txt_AAD17
+            "AAD20" -> txt_AAD20
+            "AAD23" -> txt_AAD23
+            "AAD28" -> txt_AAD28
+            "ABD4" -> txt_ABD4
+            "ABD5" -> txt_ABD5
+            "ABD9" -> txt_ABD9
+            "ABD12" -> txt_ABD12
+            "ABD17" -> txt_ABD17
+            "ABD20" -> txt_ABD20
+            "ABD23" -> txt_ABD23
+            "ABD28" -> txt_ABD28
+            else -> arrayOf()
+        }
+    }
+
+    fun getCasualStyle(style: String): IntArray {
+        return when (style) {
+            "AAA4" -> AAA4
+            "AAA5" -> AAA5
+            "AAA9" -> AAA9
+            "AAA12" -> AAA12
+            "AAA17" -> AAA17
+            "AAA20" -> AAA20
+            "AAA23" -> AAA23
+            "AAA28" -> AAA28
+            "ABA4" -> ABA4
+            "ABA5" -> ABA5
+            "ABA9" -> ABA9
+            "ABA12" -> ABA12
+            "ABA17" -> ABA17
+            "ABA20" -> ABA20
+            "ABA23" -> ABA23
+            "ABA28" -> ABA28
+            else -> intArrayOf()
+        }
+    }
+
+    fun getCasualStyleText(style: String): Array<String> {
+        return when (style) {
+            "AAA4" -> txt_AAA4
+            "AAA5" -> txt_AAA5
+            "AAA9" -> txt_AAA9
+            "AAA12" -> txt_AAA12
+            "AAA17" -> txt_AAA17
+            "AAA20" -> txt_AAA20
+            "AAA23" -> txt_AAA23
+            "AAA28" -> txt_AAA28
+            "ABA4" -> txt_ABA4
+            "ABA5" -> txt_ABA5
+            "ABA9" -> txt_ABA9
+            "ABA12" -> txt_ABA12
+            "ABA17" -> txt_ABA17
+            "ABA20" -> txt_ABA20
+            "ABA23" -> txt_ABA23
+            "ABA28" -> txt_ABA28
+            else -> arrayOf()
+        }
+    }
+
+    fun getVintageStyle(style: String): IntArray {
+        return when (style) {
+            "AAB4" -> AAB4
+            "AAB5" -> AAB5
+            "AAB9" -> AAB9
+            "AAB12" -> AAB12
+            "AAB17" -> AAB17
+            "AAB20" -> AAB20
+            "AAB23" -> AAB23
+            "AAB28" -> AAB28
+            "ABB4" -> ABB4
+            "ABB5" -> ABB5
+            "ABB9" -> ABB9
+            "ABB12" -> ABB12
+            "ABB17" -> ABB17
+            "ABB20" -> ABB20
+            "ABB23" -> ABB23
+            "ABB28" -> ABB28
+            else -> intArrayOf()
+        }
+    }
+
+    fun getVintageStyleText(style: String): Array<String> {
+        return when (style) {
+            "AAB4" -> txt_AAB4
+            "AAB5" -> txt_AAB5
+            "AAB9" -> txt_AAB9
+            "AAB12" -> txt_AAB12
+            "AAB17" -> txt_AAB17
+            "AAB20" -> txt_AAB20
+            "AAB23" -> txt_AAB23
+            "AAB28" -> txt_AAB28
+            "ABB4" -> txt_ABB4
+            "ABB5" -> txt_ABB5
+            "ABB9" -> txt_ABB9
+            "ABB12" -> txt_ABB12
+            "ABB17" -> txt_ABB17
+            "ABB20" -> txt_ABB20
+            "ABB23" -> txt_ABB23
+            "ABB28" -> txt_ABB28
+            else -> arrayOf()
+        }
+    }
+
+    fun getSportyStyle(style: String): IntArray {
+        return when (style) {
+            "AAE4" -> AAE4
+            "AAE5" -> AAE5
+            "AAE9" -> AAE9
+            "AAE12" -> AAE12
+            "AAE17" -> AAE17
+            "AAE20" -> AAE20
+            "AAE23" -> AAE23
+            "AAE28" -> AAE28
+            "ABE4" -> ABE4
+            "ABE5" -> ABE5
+            "ABE9" -> ABE9
+            "ABE12" -> ABE12
+            "ABE17" -> ABE17
+            "ABE20" -> ABE20
+            "ABE23" -> ABE23
+            "ABE28" -> ABE28
+            else -> intArrayOf()
+        }
+    }
+
+    fun getSportyStyleText(style: String): Array<String> {
+        return when (style) {
+            "AAE4" -> txt_AAE4
+            "AAE5" -> txt_AAE5
+            "AAE9" -> txt_AAE9
+            "AAE12" -> txt_AAE12
+            "AAE17" -> txt_AAE17
+            "AAE20" -> txt_AAE20
+            "AAE23" -> txt_AAE23
+            "AAE28" -> txt_AAE28
+            "ABE4" -> txt_ABE4
+            "ABE5" -> txt_ABE5
+            "ABE9" -> txt_ABE9
+            "ABE12" -> txt_ABE12
+            "ABE17" -> txt_ABE17
+            "ABE20" -> txt_ABE20
+            "ABE23" -> txt_ABE23
+            "ABE28" -> txt_ABE28
+            else -> arrayOf()
+        }
+    }
+
+    fun getStreetStyle(style: String): IntArray {
+        return when (style) {
+            "AAC4" -> AAC4
+            "AAC5" -> AAC5
+            "AAC9" -> AAC9
+            "AAC12" -> AAC12
+            "AAC17" -> AAC17
+            "AAC20" -> AAC20
+            "AAC23" -> AAC23
+            "AAC28" -> AAC28
+            "ABC4" -> ABC4
+            "ABC5" -> ABC5
+            "ABC9" -> ABC9
+            "ABC12" -> ABC12
+            "ABC17" -> ABC17
+            "ABC20" -> ABC20
+            "ABC23" -> ABC23
+            "ABC28" -> ABC28
+            else -> intArrayOf()
+        }
+    }
+
+    fun getStreetStyleText(style: String): Array<String> {
+        return when (style) {
+            "AAC4" -> txt_AAC4
+            "AAC5" -> txt_AAC5
+            "AAC9" -> txt_AAC9
+            "AAC12" -> txt_AAC12
+            "AAC17" -> txt_AAC17
+            "AAC20" -> txt_AAC20
+            "AAC23" -> txt_AAC23
+            "AAC28" -> txt_AAC28
+            "ABC4" -> txt_ABC4
+            "ABC5" -> txt_ABC5
+            "ABC9" -> txt_ABC9
+            "ABC12" -> txt_ABC12
+            "ABC17" -> txt_ABC17
+            "ABC20" -> txt_ABC20
+            "ABC23" -> txt_ABC23
+            "ABC28" -> txt_ABC28
+            else -> arrayOf()
+        }
+    }
+
+    //***************************************************************************************************
     //4(),5(),9(),12(),17(),20(),23(),28()
     // TODO : 댄디 스타일.
     // TODO : 남자  더위많이탐 댄디 4도
@@ -235,7 +458,7 @@ internal class ClothesStyleMan {
 
     //***************************************************************************************************
     // TODO : 캐주얼 스타일.
-    // TODO : 남자  더위많이탐 캐주얼 4도
+    // 남자  더위많이탐 캐주얼 4도
     var AAA4 = intArrayOf(
         R.drawable.m_casual_rugbyshirts, R.drawable.m_casual_overman, R.drawable.m_casual_gmantoman,
         R.drawable.m_casual_longpadding, R.drawable.m_casual_sropadding, R.drawable.ic_xxx,
